a7be6514c4921e9cf68ea7b6409c75e7a8eac1da
john spurling <synec@viscous.org>
2018-02-04 17:16:21 -0800

    first test passes: pushing/popping from a ring_array

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,28 @@
+extern crate libc;
+
+mod ffi;
+
 #[cfg(test)]
 mod tests {
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn ring_array_push_pop() {
+        use ffi;
+        use std::mem;
+        use libc::c_void;
+        use std::ptr;
+
+        let ring_array = unsafe { ffi::ring_array_create(8, 64) };
+        assert_ne!(ring_array.is_null(), true);
+        let mut blort = 31337u64;
+        let blort_ptr: *mut c_void = &mut blort as *mut _ as *mut c_void;
+        let result  = unsafe { ffi::ring_array_push(blort_ptr, ring_array) };
+        assert_eq!(result, 0);
+        let mut popped: u64 = 0;
+        let popped_ptr: *mut c_void = &mut popped as *mut _ as *mut c_void;
+        let result = unsafe { ffi::ring_array_pop(popped_ptr, ring_array) };
+        assert_eq!(result, 0);
+        assert_eq!(blort, popped);
+        unsafe { ffi::ring_array_destroy(ring_array) };
     }
 }
+
