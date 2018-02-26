3c9fc5c24e2d77bef4d43e0ecec5a4a5ebce368b
john spurling <synec@viscous.org>
2018-02-26 07:26:53 -0800

    * git clone ccommon from build.rs, and build it there. Much easier now.

--- build.rs
+++ build.rs
@@ -1,11 +1,30 @@
 extern crate bindgen;
+extern crate cmake;
 
 use std::env;
 use std::path::PathBuf;
+use std::process::Command;
 
 fn main() {
+    let git_cmd_output = Command::new("git")
+        .args(&["clone", "https://github.com/twitter/ccommon.git"])
+        .output().unwrap();
+
+    let path_to_ccommon = "ccommon";
+    let mut dst = cmake::Config::new(path_to_ccommon)
+        .build();
+    dst.push("lib");
+
+    println!("cargo:rustc-link-search=native={}", dst.display());
+
     // Tell cargo to tell rustc to link ccommon.
     println!("cargo:rustc-link-lib=ccommon");
+
+    let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
+    let include_path = out_path.join("include/ccommon-1.2");
+    let config_h = out_path.join("build/config.h");
+    // hack: copy config.h to the include directory.
+    std::fs::copy(config_h, &include_path.join("config.h")).unwrap();
 
     // The bindgen::Builder is the main entry point
     // to bindgen, and lets you build up options for
@@ -14,6 +33,7 @@ fn main() {
         // The input header we would like to generate
         // bindings for.
         .header("wrapper.h")
+        .clang_arg(format!("-I{}", include_path.display()))
         // Skip layout tests for now.
         .layout_tests(false)
         .blacklist_type("IPPORT_RESERVED")
@@ -24,7 +44,6 @@ fn main() {
         .expect("Unable to generate bindings");
 
     // Write the bindings to the $OUT_DIR/bindings.rs file.
-    let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
     bindings
         .write_to_file(out_path.join("bindings.rs"))
         .expect("Couldn't write bindings!");
